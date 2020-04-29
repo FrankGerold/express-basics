3e401b1821cec00c31d533b052aca067685135d0
Frank Gerold <FrankETSGerold@gmail.com>
2020-04-29 18:42:14 -0400

    Implement handlebars templates and partials, added 404

--- src/app.js
+++ src/app.js
@@ -1,28 +1,67 @@
 const path = require('path')
+
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+// Define paths for Express config
+const viewsPath = path.join(__dirname, '../templates/views')
 const publicPath = path.join(__dirname, '../public')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+// Set up static directory to serve
 app.use(express.static(publicPath))
 
-// app.get('', (request, response) => {
-//     response.send('Ayyy lmao')
-// })
 
-// app.get('/help', (request, response) => {
-//     response.send('Help Page')
-// })
-//
-// app.get('/about', (request, response) => {
-//     response.send('<h2>ABout this app</h2>')
-// })
+// Define routes
+app.get('', (request, response) => {
+    response.render('index', {
+      title: 'Weather App',
+      name: 'Frankie G'
+    })
+})
+
+app.get('/help', (request, response) => {
+    response.render('help', {
+      message: 'how to use the app 1. type name',
+      name: 'frank',
+      title: 'Help Page'
+    })
+})
+
+app.get('/about', (request, response) => {
+    response.render('about', {
+      title: 'about this app',
+      name: 'frankie g'
+    })
+})
 
 app.get('/weather', (request, response) => {
     response.send({
         location: 'HOME',
         forecast: 'cold and dank'
+    })
+})
+
+app.get('/help/*', (request, response) => {
+    response.render('404', {
+      message: 'help article not found',
+      title: 'Help Page 404'
+    })
+})
+
+app.get('*', (request, response) => {
+    response.render('404', {
+      message: 'Page not found.',
+      title: '404'
     })
 })
 
