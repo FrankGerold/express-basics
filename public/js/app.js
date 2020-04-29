1e518dc8a038217b8d6f9a82665292b468ebcd81
Frank Gerold <FrankETSGerold@gmail.com>
2020-04-29 17:52:48 -0400

    filesystem layout, static pages, styles, scripts

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log(`Vlient-Side JS Loaded!`);
--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
-app.get('', (request, response) => {
-    response.send('Ayyy lmao')
-})
+const publicPath = path.join(__dirname, '../public')
 
-app.get('/help', (request, response) => {
-    response.send('Help Page')
+app.use(express.static(publicPath))
+
+// app.get('', (request, response) => {
+//     response.send('Ayyy lmao')
+// })
+
+// app.get('/help', (request, response) => {
+//     response.send('Help Page')
+// })
+//
+// app.get('/about', (request, response) => {
+//     response.send('<h2>ABout this app</h2>')
+// })
+
+app.get('/weather', (request, response) => {
+    response.send({
+        location: 'HOME',
+        forecast: 'cold and dank'
+    })
 })
 
 app.listen(3002, () => console.log('Server running on port 3002!'))
