c72d51a5e6f503155b51c0522447fe62622a842d
Frank Gerold <FrankETSGerold@gmail.com>
2020-04-29 13:20:24 -0400

    yarn dependencies, server runs on 3002

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-Ayyy lmao
+const express = require('express')
+
+const app = express()
+
+app.get('', (request, response) => {
+    response.send('Ayyy lmao')
+})
+
+app.get('/help', (request, response) => {
+    response.send('Help Page')
+})
+
+app.listen(3002, () => console.log('Server running on port 3002!'))
