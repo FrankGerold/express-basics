b3c38879a14a57f0a4ad83f4417eada78bf5fdae
Frank Gerold <FrankETSGerold@gmail.com>
2020-04-30 17:45:53 -0400

    sloppy form works and displays weather

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,35 @@
 console.log(`Vlient-Side JS Loaded!`);
+
+const weatherSearch = (query) => {
+  return fetch(`http://localhost:3002/weather?address=${query}`)
+  .then(r => r.json())
+  .then(displayForecast)
+}
+
+const displayForecast = (weatherData) => {
+    if (weatherData.error) {
+      body.innerText = 'Please submit an actual location!'
+    }
+    else {
+      body.innerHTML = `<div class='forecast'><h2>${weatherData.location}</h2>
+      <p>${weatherData.full}</p>`
+    }
+}
+
+
+const weatherForm = document.querySelector('.weather-form')
+const locationField = document.querySelector('.location-field')
+
+const body = document.querySelector('.main-text')
+
+weatherForm.addEventListener('submit', event => {
+   event.preventDefault()
+   let loc = locationField.value
+   console.log('query:', loc);
+   if (!loc) {
+     console.log('please type a location');
+   }
+   else {
+     weatherSearch(loc)
+   }
+})
